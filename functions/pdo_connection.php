e5ed17f9755be8875c38fdac27354fce49df15b9
Mehdi.ZA <mahdi_zarei78@yahoo.com>
2023-03-15 21:07:50 +0330

    add pdo_connection

---
+++ functions/pdo_connection.php
@@ -0,0 +1,11 @@
+<?php
+global $pdo;
+try {
+    $options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ);
+    $pdo = new PDO("mysql:host='localhost';dbName='php_project';", 'root', '', $options);
+    return $pdo;
+} catch (PDOException $e) {
+    echo 'ERROR : ' . $e->getMessage();
+    exit();
+}
+?>
