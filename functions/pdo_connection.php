a4db0af7e62f974450f446f42572dd9263f569b5
Mehdi.ZA <mahdi_zarei78@yahoo.com>
2023-03-18 18:40:16 +0330

    add category panel

--- functions/pdo_connection.php
+++ functions/pdo_connection.php
@@ -2,7 +2,7 @@
 global $pdo;
 try {
     $options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ);
-    $pdo = new PDO("mysql:host='localhost';dbName='php_project';", 'root', '', $options);
+    $pdo = new PDO("mysql:host=localhost;dbName=php_project;", 'root', '', $options);
     return $pdo;
 } catch (PDOException $e) {
     echo 'ERROR : ' . $e->getMessage();
