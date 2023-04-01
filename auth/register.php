8794bce08f16a8d89231ad44c44b7651450b1d2d
Mehdi.ZA <mahdi_zarei78@yahoo.com>
2023-04-02 00:31:23 +0430

    add post to index page

--- auth/register.php
+++ auth/register.php
@@ -48,7 +48,7 @@ if (
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>PHP tutorial</title>
     <link rel="stylesheet" href="<?= asset('assets/css/bootstrap.min.css') ?>" media="all" type="text/css">
-    <link rel="stylesheet" href="<?= asset('assets/css/style.css') ?>" </head>
+    <link rel="stylesheet" href="<?= asset('assets/css/style.css') ?>"> </head>
 
 <body>
     <section id="app">
